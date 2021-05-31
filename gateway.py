bcccf3f9c311adac7b702a729feb61caf946f991
Daniel Benazzi <daniel.benazzi@studio.unibo.it>
2021-05-31 15:41:02 +0200

    update send to server function and fix thread synchronization problem in gateway

--- gateway.py
+++ gateway.py
@@ -12,16 +12,19 @@ from sys import exit
 # flags
 __stop = False
 
+# for server connection
+__server_ip = "10.10.10.10"  # 11 characters
+__server_mac = "AA:BB:CC:DD:EE:C1"  # 17 characters
+__server_port = 51_000
+
 # connection towards clients
 __gateway_client_ip = "192.168.1.01"  # 12 characters
 __gateway_client_mac = "AA:BB:CC:DD:EE:B1"  # 17 characters
 __udp_address = ("127.0.0.1", 50_000)
 
 # connection towards server
-__gateway_client_ip = "192.168.1.01"  # 12 characters
-__gateway_client_mac = "AA:BB:CC:DD:EE:B1"  # 17 characters
-__tcp_address = ("127.0.0.1", 51_000)
-
+__gateway_server_ip = "10.10.10.1"  # 10 characters
+__gateway_server_mac = "AA:BB:CC:DD:EE:B2"  # 17 characters
 
 # pairs of: (client IP address, client message)
 __client_message_map = {
@@ -32,33 +35,45 @@ __client_message_map = {
 }
 __clients_number = 1  # number of IoT client devices to serve
 
-# lock to limit access to the served clients counter
+# lock used to limit access to __clients_served counter
 __clients_lock = thr.Lock()
 __clients_served = 0
+
+# event to start the send_to_server thread
+__send_wait = thr.Event()
+__send_end = thr.Event()
 
 
 def process_message(message, index):
     global __client_message_map
     global __clients_lock
     global __clients_served
+    global __send_wait
+    global __send_end
     # format message and save it in dictionary
     message = msh.unpack_message(message)
     to_save = ""
-    # adds the source ip in each line of the message
-    for line in message.splitlines():
-        to_save += f"{index} - {line}\n"
-    __client_message_map[index] = to_save
-    # if dictionary contains 4 formatted messages launch send to server
     with __clients_lock:
+        # adds the source ip in each line of the message
+        for line in message.splitlines():
+            to_save += f"{index} - {line}\n"
+        __client_message_map[index] = to_save
+        # if dictionary contains 4 formatted messages launch send to server
         __clients_served += 1
         if __clients_served == __clients_number:
+            print(f"received a message from all {__clients_number} clients")
             # send data to server
+            __send_end.clear()  # prepare to wait for the send to complete
+            __send_wait.set()  # unlocks the send_to_server thread
+            __send_end.wait()  # wait for send to complete
+            # restore counter and dictionary
             __clients_served = 0
+            for key in __client_message_map:
+                __client_message_map[key] = ""
 
 
 # function wich listens for messages on UDP port
 def receive_from_device(connection):
-
     try:
         # endless loop to receive messages
         while True:
@@ -82,8 +97,28 @@ def receive_from_device(connection):
         connection.close()
 
 
-def send_to_server(connection, message):
-    pass
+# sends the message to the server containing the values from the IoT clients
+# stored in __client_message_map
+def send_to_server(connection):
+    global __send_wait
+    global __send_end
+    while True:
+        __send_wait.wait()  # waits until the event is unlocked
+        # make message and add "MAC" and "TCP" headers
+        message = ""
+        for value in __client_message_map.values():
+            message += value + "\n"
+        message = msh.make_message(
+            message,
+            (__gateway_server_mac, __server_mac),
+            (__gateway_server_ip, __server_ip),
+        ).encode("utf-8")
+        # open connection
+        # send message
+        print("sending message to server")
+        print(message)
+        __send_wait.clear()  # locks the send event
+        __send_end.set()  # unlocks calling thread
 
 
 if __name__ == "__main__":
@@ -98,14 +133,19 @@ if __name__ == "__main__":
         print("an exception has occured: ", e)
         exit()
 
-    # create and start thread for UDP socket
+    # create and strt thread for TCP connection
+    send_thread = thr.Thread(target=send_to_server, args=(tcp_socket,), daemon=True)
+    # create and start thread for UDP socket listening
     receive_thread = thr.Thread(target=receive_from_device, args=(udp_socket,))
     try:
+        send_thread.start()
         receive_thread.start()
         receive_thread.join()
+        send_thread.join()
     except Exception as e:
         print("an exception has occured: ", e)
     finally:
+        tcp_socket.close()
         udp_socket.close()
 
     tcp_socket.close()
